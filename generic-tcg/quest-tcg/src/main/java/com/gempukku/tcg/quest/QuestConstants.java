95bc2062de5205b5e98ad1471f03c6d4b9807318
marcins78 <marcins78@296cde9c-574c-ef92-a173-c4d016eb44a3>
2012-10-25 16:07:17 +0000

    Working on cards and zones.

--- generic-tcg/quest-tcg/src/main/java/com/gempukku/tcg/quest/QuestConstants.java
+++ generic-tcg/quest-tcg/src/main/java/com/gempukku/tcg/quest/QuestConstants.java
@@ -2,7 +2,7 @@ package com.gempukku.tcg.quest;
 
 import com.gempukku.tcg.generic.mtg.MtgConstants;
 
-public class QuestConstants extends MtgConstants{
+public class QuestConstants extends MtgConstants {
     public static final String CARD_ID_MANAGER = "cardIdManager";
 
     public static final String PLAY = "play";
