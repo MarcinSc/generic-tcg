238bddde2c66ccfec89787cc9facbfbd7bc09f30
marcins78 <marcins78@296cde9c-574c-ef92-a173-c4d016eb44a3>
2014-09-04 16:52:10 +0000

    Progress on Overpower TCG

---
+++ generic-tcg/generic-tcg-default/src/main/java/com/gempukku/tcg/generic/effect/IncrementContextAttributeEffect.java
@@ -0,0 +1,21 @@
+package com.gempukku.tcg.generic.effect;
+
+import com.gempukku.tcg.GameObjects;
+import com.gempukku.tcg.generic.action.GameActionContext;
+import com.gempukku.tcg.generic.evaluator.StringEvaluator;
+
+public class IncrementContextAttributeEffect implements GameEffect {
+    private StringEvaluator _attributeName;
+
+    public void setAttributeName(StringEvaluator attributeName) {
+        _attributeName = attributeName;
+    }
+
+    @Override
+    public Result execute(GameObjects gameObjects, GameActionContext context) {
+        String attributeName = _attributeName.getValue(gameObjects, context);
+        int value = Integer.parseInt(attributeName);
+        context.setAttribute(attributeName, String.valueOf(value+1));
+        return new Result(null, false);
+    }
+}
